221e2fad6cd95d08032413b2c48273a87ffd325c
Malav Shah <malavhs@gatech.edu>
2019-03-06 18:21:14 -0500

    Added EditUserActivity

---
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/EditUserActivity.java
@@ -0,0 +1,13 @@
+package com.example.pttmobile4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}
