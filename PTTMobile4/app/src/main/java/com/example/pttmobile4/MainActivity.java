665e77602337976ca671892b0b8f253d0afb1ceb
karankishinani <karan_kishinani@hotmail.com>
2019-03-06 14:50:10 -0500

    MainActivity + more

--- PTTMobile4/app/src/main/java/com/example/pttmobile4/MainActivity.java
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pttmobile4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,14 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainAdminLoginBtn = findViewById(R.id.mainAdminLoginBtn);
+
+       mainAdminLoginBtn = findViewById(R.id.mainAdminLoginBtn);
         mainAdminLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Send Itent
+                Intent intent = new Intent(getApplicationContext(), AdminActivity.class);
+                startActivity(intent);
+                // Send Intent
 
             }
         });
+
+        mainUserLoginBtn = findViewById(R.id.mainUserLoginBtn);
         mainUserLoginBtn.setOnClickListener(new View.OnClickListener() {
             public void  onClick(View v){
                 // Send Intent
