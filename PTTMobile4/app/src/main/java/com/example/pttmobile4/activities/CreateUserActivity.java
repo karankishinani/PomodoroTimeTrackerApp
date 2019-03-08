3037e92d48ba90202974b4618a673663321f4e6c
Bosheng Jian <bosheng@gatech.edu>
2019-03-08 15:02:40 -0500

    add callback

--- PTTMobile4/app/src/main/java/com/example/pttmobile4/activities/CreateUserActivity.java
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/activities/CreateUserActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.pttmobile4.R;
 import com.example.pttmobile4.api.Client;
 import com.example.pttmobile4.models.UserResponse;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CreateUserActivity extends AppCompatActivity {
 
@@ -31,6 +34,27 @@ public class CreateUserActivity extends AppCompatActivity {
                 // CREATE User and add them in the DB
                 Call<UserResponse> call = Client
                         .getInstance().getApi().createUser(fName.getText().toString(),lName.getText().toString(),emailId.getText().toString());
+
+                call.enqueue(new Callback<UserResponse>() {
+                    @Override
+                    public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                        UserResponse userResponse = response.body();
+
+                        if (!userResponse.isError()) {
+
+                            Toast.makeText(CreateUserActivity.this, "Something WRONG", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(CreateUserActivity.this,  "Created: " + userResponse.getUsers().getEmail(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserResponse> call, Throwable t) {
+
+                    }
+                });
+
                 // Go back to Last Activity
                 finish();
             }
