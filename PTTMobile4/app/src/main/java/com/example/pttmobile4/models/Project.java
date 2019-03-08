ddff2287046f0a1587f15be0af670de86279cfcf
Bosheng Jian <bosheng@gatech.edu>
2019-03-08 14:34:20 -0500

    add constructor and getter

--- PTTMobile4/app/src/main/java/com/example/pttmobile4/models/Project.java
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/models/Project.java
@@ -1,4 +1,24 @@
 package com.example.pttmobile4.models;
 
 public class Project {
+    int id, userId;
+    String projectname;
+
+    public Project(int id, int userId, String projectname) {
+        this.id = id;
+        this.userId = userId;
+        this.projectname = projectname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getProjectname() {
+        return projectname;
+    }
 }
--- PTTMobile4/app/src/main/java/com/example/pttmobile4/models/User.java
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/models/User.java
@@ -1,4 +1,29 @@
 package com.example.pttmobile4.models;
 
 public class User {
+    int id;
+    String email, firstName, lastName;
+
+    public User(int id, String email, String firstName, String lastName) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
