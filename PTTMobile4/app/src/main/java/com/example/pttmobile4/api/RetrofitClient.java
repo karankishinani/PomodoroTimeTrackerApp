a03653ad7c6ce98af9644715d3ac9a1ad6127208
karankishinani <karan_kishinani@hotmail.com>
2019-03-08 14:14:23 -0500

    Add RetrofitClient

---
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/api/RetrofitClient.java
@@ -0,0 +1,57 @@
+package com.example.pttmobile4.api;
+
+import android.util.Base64;
+import java.io.IOException;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String AUTH = "Basic " + Base64.encodeToString(("").getBytes(), Base64.NO_WRAP);
+
+    private static final String BASE_URL = "http://locahost:8080/ptt/api";
+    private static RetrofitClient mInstance;
+    private Retrofit retrofit;
+
+
+    private RetrofitClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(
+                        new Interceptor() {
+                            @Override
+                            public Response intercept(Chain chain) throws IOException {
+                                Request original = chain.request();
+
+                                Request.Builder requestBuilder = original.newBuilder()
+                                        .addHeader("Authorization", AUTH)
+                                        .method(original.method(), original.body());
+
+                                Request request = requestBuilder.build();
+                                return chain.proceed(request);
+                            }
+                        }
+                ).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if (mInstance == null) {
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+    }
+
+    public Api getApi() {
+        return retrofit.create(Api.class);
+    }
+
+}
