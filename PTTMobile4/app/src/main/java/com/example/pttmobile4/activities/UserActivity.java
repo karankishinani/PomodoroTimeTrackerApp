c674f68705956da9d0f91673e86fcacda75821f1
Bosheng Jian <bosheng@gatech.edu>
2019-03-11 15:38:56 -0400

    Add proj_item, project adapter, edit user load project

--- PTTMobile4/app/src/main/java/com/example/pttmobile4/activities/UserActivity.java
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/activities/UserActivity.java
@@ -3,13 +3,34 @@ package com.example.pttmobile4.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.pttmobile4.R;
+import com.example.pttmobile4.adapters.ProjectListAdapter;
+import com.example.pttmobile4.adapters.UserListAdapter;
+import com.example.pttmobile4.api.Client;
+import com.example.pttmobile4.models.Project;
+import com.example.pttmobile4.models.User;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserActivity extends AppCompatActivity {
+
+    ArrayList<Project> projectList = new ArrayList<>();
+
+    private RecyclerView mProjectList;
+    private RecyclerView.Adapter mPojectListAdapter;
+    private RecyclerView.LayoutManager mProjectListLayoutManager;
 
     Button userLogoutBtn;
     Button createProjectBtn;
@@ -20,6 +41,7 @@ public class UserActivity extends AppCompatActivity {
 
         userLogoutBtn = findViewById(R.id.userLogoutBtn);
         createProjectBtn = findViewById(R.id.createProjectBtn);
+        mProjectList = findViewById(R.id.projectList);
 
         userLogoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +58,42 @@ public class UserActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void loadProjects(){
+        // GET REQUEST to populate fields initially
+        // TODO: user ID for projects
+        Call<ArrayList<Project>> call = Client
+                .getInstance().getApi().getProjects(1);
+
+        call.enqueue(new Callback<ArrayList<Project>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Project>> call, Response<ArrayList<Project>> response) {
+                projectList = response.body();
+                if (projectList==null){
+                    System.out.println("Projectlist is null");
+                }
+                else {
+                    loadRecyclerView();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Project>> call, Throwable t) {
+            }
+
+        });
+    }
+    private void loadRecyclerView() {
+        mProjectList.setNestedScrollingEnabled(false);
+        mProjectList.setHasFixedSize(false);
+        mProjectListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL, false);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,
+                LinearLayout.VERTICAL);
+        mProjectList.addItemDecoration(dividerItemDecoration);
+        mProjectList.setLayoutManager(mProjectListLayoutManager);
+        mPojectListAdapter = new ProjectListAdapter(this, projectList);
+        mProjectList.setAdapter(mPojectListAdapter);
+        mPojectListAdapter.notifyDataSetChanged();
     }
 }
---
+++ PTTMobile4/app/src/main/java/com/example/pttmobile4/adapters/ProjectListAdapter.java
@@ -0,0 +1,128 @@
+package com.example.pttmobile4.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pttmobile4.R;
+import com.example.pttmobile4.activities.AdminActivity;
+import com.example.pttmobile4.activities.CreateProjectActivity;
+import com.example.pttmobile4.activities.CreateProjectActivity;
+import com.example.pttmobile4.activities.EditProjectActivity;
+import com.example.pttmobile4.models.Project;
+
+import java.util.ArrayList;
+
+public class ProjectListAdapter extends RecyclerView.Adapter<ProjectListAdapter.ProjectListViewHolder> {
+
+    Context context;
+    private ArrayList<Project> projectList;
+    private boolean selectingMultipleProjects = false;
+
+
+    public ProjectListAdapter(Context context, ArrayList<Project> projectList){
+        this.projectList = projectList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProjectListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_item, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutView.setLayoutParams(lp);
+
+        ProjectListViewHolder rcv = new ProjectListViewHolder(layoutView);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ProjectListViewHolder holder, final int position) {
+        holder.mProjectName.setText(projectList.get(position).getProjectname());
+
+        holder.mLayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+//                updateProjectClick(holder);
+                return true;
+            }
+        });
+        holder.mLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectingMultipleProjects){
+//                    updateProjectClick(holder);
+                    Toast.makeText((AdminActivity)context,  "Selected multiple projects ", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    int pos = holder.getAdapterPosition();
+                    // todo:
+                    try {
+                        int id = projectList.get(pos).getId();
+                        if (id == 0){
+                            Toast.makeText((AdminActivity)context,  "This Project's id is 0!!!!!!!!! ", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    String id = "" + projectList.get(pos).getId();
+                    Intent intent = new Intent(context, EditProjectActivity.class);
+                    intent.putExtra("USER_ID",id);
+                    context.startActivity(intent);
+
+//                    projectList.get(position).setSelected(true);
+//                    ((FindProjectActivity)context).createChat();
+                }
+
+            }
+        });
+    }
+
+//    private void updateProjectClick(ProjectListViewHolder holder){
+//        int position = holder.getAdapterPosition();
+//
+//        projectList.get(position).setSelected(!projectList.get(position).getSelected());
+//
+//        if(projectList.get(position).getSelected())
+//            holder.mSelected.setBackground(context.getResources().getDrawable(R.drawable.ic_check));
+//        else
+//            holder.mSelected.setBackground(null);
+//
+//        updateSelectingMultipleProjects();
+//    }
+
+//    private void updateSelectingMultipleProjects() {
+//        for(Project mProject : projectList){
+//            if(mProject.getSelected()){
+//                selectingMultipleProjects = true;
+//                return;
+//            }
+//        }
+//        selectingMultipleProjects = false;
+//    }
+
+    @Override
+    public int getItemCount() {
+        return projectList.size();
+    }
+
+
+    class ProjectListViewHolder extends RecyclerView.ViewHolder{
+        TextView mProjectName;
+        LinearLayout mLayout;
+        ProjectListViewHolder(View view){
+            super(view);
+            mProjectName = view.findViewById(R.id.project_name);
+            mLayout = view.findViewById(R.id.layout);
+        }
+    }
+}
