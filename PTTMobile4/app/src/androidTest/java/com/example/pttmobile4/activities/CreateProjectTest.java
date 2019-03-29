12c2a267cfc1cccf3864058733a0a35974c1dad8
karankishinani <karan_kishinani@hotmail.com>
2019-03-29 14:04:06 -0400

    new changes test

--- PTTMobile4/app/src/androidTest/java/com/example/pttmobile4/activities/CreateProjectTest.java
+++ PTTMobile4/app/src/androidTest/java/com/example/pttmobile4/activities/CreateProjectTest.java
@@ -36,7 +36,7 @@ import static org.hamcrest.Matchers.is;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class CreateProjectTest {
-
+//
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
